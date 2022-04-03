2e6511563e73ea93f30fc7ffb1a77e9412a2c60e
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2022-04-03 17:35:20 -0400

    Initial client skeleton using React and antd (client)

---
+++ client/src/modules/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Layout } from 'antd'
+
+import Navbar from "../../components/Navbar";
+
+const { Header, Content } = Layout;
+
+const Home = () => {
+  return (
+    <>
+      <Navbar/>
+    </>
+  );
+};
+
+export default Home;
