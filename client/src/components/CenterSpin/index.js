bf7269c9d11b9ca40596201e5cf6caac472b2470
Eduardo Miguel Hernandez <emiguelc1991@protonmail.com>
2022-04-04 16:56:44 -0400

    Added center spinner component (client)

---
+++ client/src/components/CenterSpin/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Spin } from 'antd';
+
+import './index.css'
+
+const CenterSpin = () => {
+
+  return (
+      <div className="centerSpin">
+          <Spin />
+      </div>
+  )
+}
+
+export default CenterSpin;
