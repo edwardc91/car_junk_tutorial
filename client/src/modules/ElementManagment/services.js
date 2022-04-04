32248345d0baa686e75577789f04a9699d2acb77
Eduardo Miguel Hernández <>
2022-04-04 17:45:00 -0400

    Added initial actions and service config for element managment module (client)

---
+++ client/src/modules/ElementManagment/services.js
@@ -0,0 +1,130 @@
+const axios = require("axios");
+REACT_APP_API_URI = process.env.REACT_APP_DEV_API_URL
+
+//! Makes
+export const getAllMakesService = async () => {
+    var response = await axios({
+        method: "get",
+        url: `${REACT_APP_API_URI}/Makes`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const addMakeService = async data => {
+    var response = await axios({
+        method: "post",
+        url: `${REACT_APP_API_URI}/Makes`,
+        data
+    });
+
+    if (response && response.status === 201) {
+        let data = await response.data;
+        return data
+    }
+};
+
+
+export const getMakeService = async id => {
+    var response = await axios({
+        method: "get",
+        url: `${REACT_APP_API_URI}/Makes/${id}`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const updateMakeService = async (id, data) => {
+    var response = await axios({
+        method: "put",
+        url: `${REACT_APP_API_URI}/Makes/${id}`,
+        data
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const deleteMakeService = async id => {
+    var response = await axios({
+        method: "delete",
+        url: `${REACT_APP_API_URI}/Makes/${id}`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+//! Sizes
+export const getAllSizesService = async () => {
+    var response = await axios({
+        method: "get",
+        url: `${REACT_APP_API_URI}/Siizes`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const addSizeService = async data => {
+    var response = await axios({
+        method: "post",
+        url: `${REACT_APP_API_URI}/Sizes`,
+        data
+    });
+
+    if (response && response.status === 201) {
+        let data = await response.data;
+        return data
+    }
+};
+
+
+export const getSizesService = async id => {
+    var response = await axios({
+        method: "get",
+        url: `${REACT_APP_API_URI}/Sizes/${id}`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const updateSizesService = async (id, data) => {
+    var response = await axios({
+        method: "put",
+        url: `${REACT_APP_API_URI}/Sizes/${id}`,
+        data
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
+
+export const deleteSizesService = async id => {
+    var response = await axios({
+        method: "delete",
+        url: `${REACT_APP_API_URI}/Sizes/${id}`
+    });
+
+    if (response && response.status === 200) {
+        let data = await response.data;
+        return data
+    }
+};
